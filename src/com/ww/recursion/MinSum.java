6812f70f24d3895dafff7815e02b629387c191ad
ww <ww13419571859@163.com>
2022-04-23 16:59:18 +0800

    数组小合问题

---
+++ src/com/ww/recursion/MinSum.java
@@ -0,0 +1,62 @@
+package com.ww.recursion;
+
+/**
+ * 小合问题：归并排序的扩展
+ * 给定一个数组，每个左边的数比右边的数小，把小的这些数加起来叫做这个数组的小合。
+ */
+public class MinSum {
+    public int  process(int[] arr, int left, int right) {
+        while (arr == null || arr.length < 2 || left == right) {//如果数组为空或者长度小于2不产生小合
+            return 0;
+        }
+        int mid = left + ((right - left) >> 1);//中点
+        //合并并排序，将每次产生的小合相加
+        return process(arr, left, mid) + process(arr, mid + 1, right) + merge(arr, left, mid, right);
+    }
+
+    /**
+     * 排序并计算小合//不排序会导致无法计算小合
+     * @param arr
+     * @param left
+     * @param mid
+     * @param right
+     */
+    private int merge(int[] arr, int left, int mid, int right) {
+        int[] helpArr = new int[right - left + 1];
+        int p = 0;
+        int pl = left;
+        int pr = mid + 1;
+        int res = 0;//小合
+        while (pl <= mid && pr <= right) {
+            res += arr[pl] < arr[pr] ? arr[pl] * (right - pr + 1) : 0;//如果左边比右边小产生小合，次数为右边指针到结尾的距离+1
+            helpArr[p++] = arr[pl] < arr[pr] ? arr[pl++] : arr[pr++];//谁小拷贝谁，相等先拷贝右侧
+        }
+        while (pl <= mid) {
+            helpArr[p++] = arr[pl++];
+        }
+        while (pr <= right) {
+            helpArr[p++] = arr[pr++];
+        }
+        for (int i = 0; i < helpArr.length; i++) {
+            arr[left++] = helpArr[i];
+        }
+        return res;
+    }
+
+    /**
+     * 暴力求解小合
+     * @param arr
+     * @return
+     */
+    public int getMinSun(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    sum += arr[i];
+                }
+            }
+        }
+        return sum;
+    }
+}
--- src/com/ww/test/Test.java
+++ src/com/ww/test/Test.java
@@ -1,6 +1,7 @@
 package com.ww.test;
 
 import com.ww.recursion.LocalMin;
+import com.ww.recursion.MinSum;
 import com.ww.recursion.SearchMax;
 import com.ww.sort.InsertionSort;
 import com.ww.sort.MergeSort;
@@ -12,13 +13,17 @@ import java.util.Arrays;
  */
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {2, 10, 2, 3, 4, 5, 6, 2, 9, 7, 8, 9};
+        int[] arr = {2, 10, 2, 3, 4, 5, 6, 2, 9, 7, 8, 9};//18+16+21+24+25+24+8+14+8
 //        new InsertionSort().insertionSort(arr);
 //        System.out.println(Arrays.toString(arr));
 //        System.out.println(new BinarysSearch().searchLightNum(arr,8));
 //        System.out.println(new LocalMin().searchLocalMin(arr));
 //        System.out.println(new SearchMax().process(arr,0,arr.length-1));
-        new MergeSort().process(arr,0,arr.length-1);
+//        new MergeSort().process(arr,0,arr.length-1);1
+        System.out.println(new MinSum().getMinSun(arr));
+        int sum = new MinSum().process(arr, 0, arr.length - 1);
+        System.out.println(sum);
         System.out.println(Arrays.toString(arr));
+
     }
 }
